7dfb5ee50a51316ea240fda4aeabfbb16d5ec368
santhoshkumar <santhoshkumar.ravichandran@klm.com>
2020-02-20 13:38:56 +0100

    feat: creating landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { CustomerOverviewComponent } from './components/customer-overview/customer-overview.component';
 import { BusinessOverviewComponent } from './components/business-overview/business-overview.component';
-
+import { LandingComponent } from './components/landing/landing.component';
 const routes: Routes = [
   { path: 'customer-overview', component: CustomerOverviewComponent },
-  { path: 'business-overview', component: BusinessOverviewComponent }
+  { path: 'business-overview', component: BusinessOverviewComponent }, {
+    path: '', component: LandingComponent
+  },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerOverviewComponent } from './components/customer-overview/customer-overview.component';
 import { BusinessOverviewComponent } from './components/business-overview/business-overview.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerOverviewComponent,
-    BusinessOverviewComponent
+    BusinessOverviewComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
